962577dac62c8f526c8b7aa440bc71726f214920
uselddehao <985836423@qq.com>
2021-07-30 09:36:09 +0800

    ant测试

---
+++ src/views/form/testTable/list/ceTypelIST.vue
@@ -0,0 +1,14 @@
+<template><div class="main"><h2>wowowohi</h2></div>
+</template>
+
+<script>
+  export default {
+    name: 'ceTypelIST'
+  }
+</script>
+
+<style scoped>
+  .main{
+    font-size: 30px;
+  }
+</style>
---
+++ src/views/form/testTable/list/testChilrenList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div style="text-align: center">
+        我的表单
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'testChilrenList'
+  }
+</script>
+
+<style scoped>
+  .main{
+    width: 100%;
+  }
+</style>
